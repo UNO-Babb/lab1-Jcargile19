d58e7bd934c9fd506f61e38884091793ec02fcdb
Jcargile19 <jcargile@unomaha.edu>
2024-08-30 17:14:56 +0000

    Finished Lab 1

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,21 +1,24 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name:Jacob Cargile
+#Date:8/30/2024
+#Assignment:First Program
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
-
+  name = input("Enter your name: ")
+  
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
-
+  age = input("Enter your age:")
+  age = int(age) #convert from words to numbers 
   #Tell the user what year they were born in.
+  born = 2024-age
   #Assume that they have not had their birthday yet this year.
-
+  print("You were born in", born)
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -1,15 +1,20 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Jacob Cargile
+#Date: 8/30/2024
+#Assignment: MadLib
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input("Enter your name:")
+  noun2 = input("Enter what sauce or condiment you would use:")
+  noun3 = input("What type of meat would you like:")
+  noun4 = input("1st topping for you sandwich:")
+  noun5 = input("2nd topping for your sandwich:")
+  noun6 = input("WHat type of bread would you like for your sandwich:")
   #Print the story with the user supplied words.
-
-
+  print("If I," , noun1 , ",were going to make a sandwich I would add" , noun2 , "to my bread, and use" , noun3 , "as my meat." )
+  print("I would also add" , noun4 , "and" , noun5 , "as extra toppings to finish it off. Of course the most important part is the bread and I would choose" , noun6 , ".")
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
